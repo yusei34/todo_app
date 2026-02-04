582b99e30f28e62c82e0b9e8a05ad01cc16f9781
yusei <matsu.yusei3414@gmail.com>
2026-02-04 21:29:39 +0900

    dataclassをmain.pyからmodels.pyへ分離。priorityのデフォルト値を修正した

--- todo/__main__.py
+++ todo/__main__.py
@@ -1,15 +1,3 @@
-from dataclasses import dataclass, field
-from datetime import date
-
-@dataclass
-class Todo:
-    id : int
-    title : str
-    done : bool = field(default=False)
-    created_at : date = field(default_factory=date.today)
-    due : date | None = field(default=None)
-    priority : int = field(default=3)
-    
 def main():
     pass
 
